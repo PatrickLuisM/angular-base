149a0a689feb6121127189b9629ff30fedb8f20f
Patrick Luis <patricklmo32@gmail.com>
2021-08-30 00:34:41 -0500

    primer commit

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,28 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+})
+export class HeroeComponent{
+   nombre: string = 'Spiderman'
+   edad: number = 43
+
+   get nombreCapitalizado():string{
+       return this.nombre.toUpperCase();
+   }
+
+   obtenerNombre(): string{
+       return `${this.nombre} - ${this.edad}`;
+   }
+
+   cambiarNombre():void{
+      this.nombre = 'Batman';
+   }
+
+   cambiarEdad(): void{
+       this.edad = 21;
+   }
+}
